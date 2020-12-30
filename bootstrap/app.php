f75f6a5116ceb668b4197e902fb4b66676e968d3
unknown <batkorumbawamedsos@gmail.com>
2020-12-30 13:43:29 +0700

    tambah cors module

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -84,7 +84,7 @@ $app->register(App\Providers\AuthServiceProvider::class);
 $app->register(Illuminate\Mail\MailServiceProvider::class);
 $app->register(Tymon\JWTAuth\Providers\LumenServiceProvider::class);
 $app->register(Ixudra\Curl\CurlServiceProvider::class);
-$app->register(Nord\Lumen\Cors\CorsServiceProvider);
+$app->register(Nord\Lumen\Cors\CorsServiceProvider::class);
 //$app->register(App\Providers\EventServiceProvider::class);
 
 
